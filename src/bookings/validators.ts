5cb28e8d0a01063a02838426457cfe927bf91a03
mutai001 <omusekim@gmail.com>
2025-02-04 23:12:56 +0300

    upadted bookings validators

---
+++ src/bookings/validators.ts
@@ -0,0 +1,8 @@
+import {integer } from 'drizzle-orm/pg-core';
+import { z } from 'zod';
+
+export const bookingsSchema = z.object({
+    user_id: z.number(),
+    therapist_id: z.number(),
+    session_date: z.string(),
+})
